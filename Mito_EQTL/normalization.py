a343318a30bf6ce54294569aa285fb86262eb780
Brandon Khuu <khuub@uci.edu>
2021-03-02 21:35:26 -0500

    Data Cleaning
    Different Normalization methods

--- Mito_EQTL/normalization.py
+++ Mito_EQTL/normalization.py
@@ -2,12 +2,18 @@
 import numpy as np 
 import pandas as pd
 from sklearn.preprocessing import StandardScaler 
+from sklearn.preprocessing import Normalizer
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import RobustScaler
 
-count_exp = pd.read_csv('count_expression_no_index.csv')
+count_exp = pd.read_csv('count_expression.csv')
 fpkm_exp = pd.read_csv('fpkm_expression.csv')
 
 scaler = StandardScaler()
-'''
+normalizer = Normalizer()
+minmax = MinMaxScaler()
+robust = RobustScaler()
+
 #################################################
 count_exp['Mean'] = count_exp.mean(axis=1)
 l = len(count_exp.iloc[:,0])
@@ -19,13 +25,22 @@ for i in range(l):
 
 count_exp_edit = count_exp.drop(lst)
 count_exp_edit = count_exp_edit.drop('Mean', axis=1)
-count_exp_edit.to_csv('count_expression_no_zero.csv')
+#count_exp_edit.to_csv('count_expression_no_zero.csv')
 
+count_exp_minmax = pd.DataFrame(minmax.fit_transform(count_exp_edit), columns = count_exp_edit.columns)
+count_exp_norm = pd.DataFrame(normalizer.fit_transform(count_exp_edit), columns=count_exp_edit.columns)
+count_exp_robust = pd.DataFrame(robust.fit_transform(count_exp_edit), columns = count_exp_edit.columns)
+
+count_exp_minmax.to_csv(r'count_expression_minmax_norm.csv')
+count_exp_norm.to_csv(r'count_expression_normalizer.csv')
+count_exp_robust.to_csv(r'count_expression_robust.csv')
+'''
 count_exp_log_norm = np.log1p(count_exp_edit)
 count_exp_standard_norm = pd.DataFrame(scaler.fit_transform(count_exp_edit), columns=count_exp_edit.columns)
 
 count_exp_log_norm.to_csv(r'count_expression_log_norm.csv')
 count_exp_standard_norm.to_csv(r'count_expression_standard_norm.csv')
+'''
 
 '''
 #####################################################
@@ -47,3 +62,4 @@ fpkm_exp_standrad_norm = pd.DataFrame(scaler.fit_transform(fpkm_exp_edit), colum
 
 fpkm_exp_log_norm.to_csv(r'fpkm_expression_log_norm.csv')
 fpkm_exp_standrad_norm.to_csv(r'fpkm_expression_standrad_norm.csv')
+'''
