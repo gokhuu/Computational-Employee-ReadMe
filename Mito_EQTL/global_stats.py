b47c1f847c27229350a6c8c15e0ce21807653f32
gokhuu <khuub@uci.edu>
2021-03-05 14:06:24 -0500

    3/5/21 attempt to clean

--- Mito_EQTL/global_stats.py
+++ Mito_EQTL/global_stats.py
@@ -28,6 +28,7 @@ def threshold_data(df,n):
 
 	return lst
 
+
 def apply_scalers(df):
 	df_standard_scaler = pd.DataFrame(standardscaler.fit_transform(df), columns=df.columns)
 	df_normalizer = pd.DataFrame(normalizer.fit_transform(df), columns=df.columns)
@@ -46,13 +47,25 @@ def log_normalization(df):
 
 	return df
 
+
 #Load Raw data
 raw_read_count_data = pd.read_csv('count_expression.csv')
 raw_fpkm_data = pd.read_csv('fpkm_expression.csv')
+raw_col_name = pd.read_csv('expression_desc.csv')
 
-#Remove random column
+
 raw_read_count_data = raw_read_count_data.drop(columns="Unnamed: 0")
-raw_fpkm_data = raw_fpkm_data.drop(columns="Unnamed: 0")
+
+
+'''
+#Add Gene name column as index
+col_name = raw_col_name.iloc[:,3]
+#raw_read_count_data.insert(0,'Gene', col_name)
+raw_read_count_data = raw_read_count_data.set_index('Gene')
+
+raw_fpkm_data.insert(0,'Gene', col_name)
+raw_fpkm_data = raw_fpkm_data.set_index('Gene')
+'''
 
 #Apply Threshold Function
 read_count_threshold_list = threshold_data(raw_read_count_data,10)
@@ -69,15 +82,34 @@ read_count_data_standard, read_count_data_normalizer, read_count_data_minmax_sca
 fpkm_data_standard, fpkm_data_normalizer, fpkm_data_minmax_scaler, fpkm_data_robust_scaler = apply_scalers(fpkm_data)
 
 #apply log2+1 normalization
-#fpkm_log = log_normalization(fpkm_data)
-#read_count_data_log = log_normalization(read_count_data)
+fpkm_log = log_normalization(fpkm_data)
+read_count_data_log = log_normalization(read_count_data)
 
 #Load log2 data
-read_count_log2 = pd.read_csv('read_count_log2_normalization.csv')
-fpkm_log2 = pd.read_csv('fpkm_expression_log2_normalization.csv')
+#read_count_log2 = pd.read_csv('read_count_log2_normalization.csv')
+#fpkm_log2 = pd.read_csv('fpkm_expression_log2_normalization.csv')
 
 
 
+#update csv files
+raw_read_count_data.to_csv(r'count_expression.csv')
+read_count_data.to_csv(r'count_expression_no_zero.csv')
+read_count_data_log.to_csv(r'count_expression_log2.csv')
+read_count_data_standard.to_csv(r'count_expression_standard.csv')
+read_count_data_normalizer.to_csv(r'count_expression_normalizer.csv')
+read_count_data_minmax_scaler.to_csv(r'count_expression_minmax_norm.csv')
+read_count_data_robust_scaler.to_csv(r'count_expression_robust.csv')
+
+raw_fpkm_data.to_csv(r'fpkm_expression.csv')
+fpkm_data.to_csv(r'fpkm_expression_no_zero.csv')
+fpkm_log.to_csv(r'fpkm_expression_log2_normalization.csv')
+fpkm_data_standard.to_csv(r'fpkm_data_standard.csv')
+fpkm_data_normalizer.to_csv(r'fpkm_expression_normalizer.csv')
+fpkm_data_minmax_scaler.to_csv(r'fpkm_expression_minmax_norm.csv')
+fpkm_data_robust_scaler.to_csv(r'fpkm_expression_robust.csv')
+
+
+'''
 #Global Stats
 #----------------------------------------------------------
 #Raw stats
@@ -86,15 +118,18 @@ raw_fpkm_data_description = basic_stats(raw_fpkm_data)
 
 #Threshold applied stats
 #n = 10
-#read_count_data_description = basic_stats(read_count_data)
-#read_count_below_threshold = len(read_count_threshold_list) #21605
+read_count_data_description = basic_stats(read_count_data)
+read_count_below_threshold = len(read_count_threshold_list) #21605
 
 #n = 2
 fpkm_data_description = basic_stats(fpkm_data)
 fpkm_below_threshold = len(fpkm_threshold_list) #26153
 
-#Sk learn Preprocessing stats
+#Log2 stats
+#read_count_log2_description = basic_stats(read_count_log2)
+#fpkm_log2_description = basic_stats(fpkm_log2)
 
+#Sk learn Preprocessing stats
 read_count_data_standard_description = basic_stats(read_count_data_standard)
 read_count_data_normalizer_description = basic_stats(read_count_data_normalizer)
 read_count_data_minmax_scaler_description = basic_stats(read_count_data_minmax_scaler)
@@ -104,3 +139,4 @@ fpkm_data_standard_description = basic_stats(fpkm_data_standard)
 fpkm_data_normalizer_description = basic_stats(fpkm_data_normalizer)
 fpkm_data_minmax_scaler_description = basic_stats(fpkm_data_minmax_scaler)
 fpkm_data_robust_scaler_description = basic_stats(fpkm_data_robust_scaler)
+'''
