e4b5cf03d075a4dc16c046e22e9cf124c999d785
Brandon Khuu <khuub@uci.edu>
2021-03-08 12:22:33 -0500

    edit_log2_file

--- Mito_EQTL/append_columns.py
+++ Mito_EQTL/append_columns.py
@@ -12,16 +12,17 @@ def gene_list(lst1, lst2):
 	return final
 
 #Load data
+'''
 #read count data
 raw_rc_data = pd.read_csv('count_expression.csv')
 rc_data = pd.read_csv('count_expression_no_zero.csv')
 rc_data_log2 = pd.read_csv('count_expression_log2.csv')
 rc_data_int = pd.read_csv('Count_expression_INT.csv')
-
+'''
 #fpkm data
 raw_fpkm_data = pd.read_csv('fpkm_expression.csv')
 fpkm_data = pd.read_csv('fpkm_expression_no_zero.csv')
-fpkm_data_log2 = pd.read_csv('fpkm_expression_log2_normalization.csv')
+fpkm_data_log2 = pd.read_csv('fpkm_expression_log2.csv')
 fpkm_data_int = pd.read_csv('fpkm_expression_INT.csv')
 
 #Load gene list
@@ -29,22 +30,24 @@ gene_data = pd.read_csv('expression_desc.csv')
 gene_id = gene_data.iloc[:,3]
 
 #threshold_list
-col = rc_data.iloc[:,0]
+#col = rc_data.iloc[:,0]
 cols = fpkm_data.iloc[:,0]
 
-rc_gene_col = gene_list(col, gene_id)
+#rc_gene_col = gene_list(col, gene_id)
 fpkm_gene_col = gene_list(cols, gene_id)
 
+'''
 raw_rc_data.insert(0, 'Gene', gene_id)
 rc_data.insert(0, 'Gene', rc_gene_col)
 rc_data_log2.insert(0, 'Gene', rc_gene_col)
 rc_data_int.insert(0,'Gene', rc_gene_col)
-
-
-print(len(fpkm_data_log2.iloc[:,0]))
 '''
+
+#print(len(fpkm_data_log2.iloc[:,0]))
+
 raw_fpkm_data.insert(0, 'Gene', gene_id)
 fpkm_data.insert(0, 'Gene', fpkm_gene_col)
 fpkm_data_log2.insert(0, 'Gene', fpkm_gene_col)
-fpkm_data_int.insert(0,'Gene', fpkm_gene_col)
-'''
+#fpkm_data_int.insert(0,'Gene', fpkm_gene_col)
+
+
--- Mito_EQTL/redo.py
+++ Mito_EQTL/redo.py
@@ -4,27 +4,20 @@ import numpy as np
 import pandas as pd 
 import math
 
-'''
-def threshold_data(df,l,n):
-	df["Mean"] = df.mean(axis=1)
-	lst = []
+def log_normalization(df):
+	dim_x = len(df.columns)
+	dim_y = len(df.iloc[:,0])
 
-	
-	for i in range(l):
-		if df.Mean[i] < n:
-			lst.append(i)
+	for i in range(dim_x):
+		for j in range(dim_y):
+			df.iloc[j,i] = math.log2(1+df.iloc[j,i])
 
-	return lst
-
-raw_fpkm = pd.read_csv('fpkm_expression.csv')
-
-l = len(raw_fpkm.iloc[:,0])
-
-x = threshold_data(raw_fpkm, l, 2)
-
-print(x)
-'''
+	return df
 
 x = pd.read_csv('fpkm_expression_no_zero.csv')
 
-print(x.iloc[:,0])
+x = x.drop('Unnamed: 0', axis=1)
+
+y = log_normalization(x)
+
+y.to_csv('fpkm_expression_log2.csv')
