088d644cb22eb2cc279d19d138f58fdf8cbd34cf
Brandon Khuu <khuub@uci.edu>
2021-03-08 11:35:26 -0500

    update fpkm data

--- Mito_EQTL/redo.py
+++ Mito_EQTL/redo.py
@@ -4,6 +4,7 @@ import numpy as np
 import pandas as pd 
 import math
 
+'''
 def threshold_data(df,l,n):
 	df["Mean"] = df.mean(axis=1)
 	lst = []
@@ -22,3 +23,8 @@ l = len(raw_fpkm.iloc[:,0])
 x = threshold_data(raw_fpkm, l, 2)
 
 print(x)
+'''
+
+x = pd.read_csv('fpkm_expression_no_zero.csv')
+
+print(x.iloc[:,0])
