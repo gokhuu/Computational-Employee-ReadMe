9a1552e66f49d4d3687d54ad1d968ccc07135b7c
Brandon Khuu <47408225+gokhuu@users.noreply.github.com>
2021-04-08 19:54:09 -0400

    cleaning files

--- Autism_edgeR/16p11_pval_hist.py
+++ Autism_edgeR/16p11_pval_hist.py
@@ -1,12 +1,12 @@
-#python3
-import numpy as np 
+# python3
+import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
 import seaborn as sns
 
-df15 = pd.read_csv('16p11_pval.csv',header=0)
+df15 = pd.read_csv('16p11_pval.csv', header=0)
 
-sns.distplot(df15['Pvalue w/ covariates'],hist=True, kde=True,bins=int(180/5),color='darkblue',hist_kws={'edgecolor':'black'},kde_kws={'linewidth':4})
+sns.distplot(df15['p_value w/ covariate'], hist=True, kde=True, bins=int(180 / 5), color='darkblue', hist_kws={'edgecolor': 'black'}, kde_kws={'linewidth': 4})
 plt.title('16p11 w/covariate pval density histogram')
 plt.xlabel('P Value')
 plt.ylabel('Density')
