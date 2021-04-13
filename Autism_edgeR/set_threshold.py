e3c6871111e6785ca3f4363f81badd56a6e96455
Brandon Khuu <khuub@uci.edu>
2021-04-13 16:12:30 -0400

    attempt edgeR 15q_dup

---
+++ Autism_edgeR/set_threshold.py
@@ -0,0 +1,19 @@
+#python3
+import numpy as np
+import pandas as pd 
+
+df15 = pd.read_csv('15q_dup_fpkm.csv',header=0)
+
+df15['Mean'] = df15.mean(axis=1)
+
+lst = []
+for i in range(df15.shape[0]):
+	if df15.Mean[i] < 2:
+		lst.append(i)
+
+df15 = df15.drop(lst)
+df15 = df15.drop('Mean',axis=1)
+
+df15 = df15.set_index('Short Gene Name')
+
+df15.to_csv(r'15q_dup_fpkm_threshold.csv')
