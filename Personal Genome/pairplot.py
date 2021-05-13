d66a8f58e5a09c5fea6324e3998ccf65ab21e9c3
Brandon Khuu <47408225+gokhuu@users.noreply.github.com>
2021-05-13 09:22:18 -0400

    heatmap

--- Personal Genome/pairplot.py
+++ Personal Genome/pairplot.py
@@ -3,9 +3,23 @@
 import numpy as np 
 import pandas as pd 
 import matplotlib.pyplot as plt 
-import seaborn as sns
 
 df = pd.read_csv('new_fpkm_threshold.csv', header=0, index_col=0, low_memory=False)
 
-sns.pairplot(df, hue='Autism')
-plt.savefig('Pairplot_fpkm.png',dpi=3000.0)
+y = df.Autism
+x = df.iloc[:,1:]
+
+cols = [i for i in x.columns]
+
+fig,ax = plt.subplots()
+im = ax.imshow(x)
+
+ax.set_xticks(range(len(cols)))
+ax.set_yticks(range(len(cols)))
+
+ax.set_xticklabels(cols)
+ax.set_yticklabels(cols)
+
+plt.setp(ax.get_xticklabels(), rotation=45, ha='right', rotation_mode='anchor')
+fig.tight_layout()
+plt.savefig('heatmap_dendro.png', dpi=4000.0)
---
+++ Personal Genome/workspace.py
@@ -0,0 +1,5 @@
+from graphviz import render
+from graphviz import Source
+
+render('dot','png','tree.dot')
+#Source.from_file('tree.dot')
