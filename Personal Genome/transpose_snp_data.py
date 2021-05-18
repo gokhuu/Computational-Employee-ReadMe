ecb20dcb66171f2c6655b829b1bd2536f459a007
Brandon Khuu <47408225+gokhuu@users.noreply.github.com>
2021-05-18 14:19:16 -0400

    model testing

--- Personal Genome/transpose_snp_data.py
+++ Personal Genome/transpose_snp_data.py
@@ -1,7 +1,7 @@
 import numpy as np 
 import pandas as pd 
 
-df = pd.read_csv('SNP_data.csv',header=0)
+df = pd.read_csv('SNP_data.csv',header=0,low_memory=False)
 
 ref = {'110-1':'BXS0110-1','110-2':'BXS0110-2','110-3':'BXS0110-3',
 		'111-1':'BXS0111-1','111-2':'BXS0111-2','111-3':'BXS0111-3',
@@ -10,6 +10,7 @@ ref = {'110-1':'BXS0110-1','110-2':'BXS0110-2','110-3':'BXS0110-3',
        '115-1':'BXS0115-1','115-2':'BXS0115-2','115-3':'BXS0115-3',
        '116-1':'BXS0116-1','116-2':'BXS0116-2','116-3':'BXS0116-3',
        '117-1':'BXS0117-1','117-2':'BXS0117-2','117-3':'BXS0117-3'}
+df = df.drop('0',axis=1)
 
 df.rename(columns=ref, inplace=True)
 chrmPos = []
@@ -21,7 +22,6 @@ for i in range(df.shape[0]):
 df.insert(loc=0, column='Samples', value=chrmPos)
 
 df = df.drop(['CHROM','POS'], axis=1)
-
-df_t=df.T
-print(df_t.head())
-df_t.to_csv(r'SNP_data_T.csv')
+df = df.set_index('Samples')
+print(df.T)
+#df_t.to_csv(r'SNP_data_T.csv')
