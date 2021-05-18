4a34af6e540361b06ddb7cbab4916a8ee5d46587
Brandon Khuu <47408225+gokhuu@users.noreply.github.com>
2021-05-18 10:44:23 -0400

    transposed SNP csv

--- Personal Genome/transpose_snp_data.py
+++ Personal Genome/transpose_snp_data.py
@@ -1,22 +1,15 @@
 import numpy as np 
 import pandas as pd 
 
-df = pd.read_csv('SNP_data.csv', header=0, index_col=0)
+df = pd.read_csv('SNP_data.csv',header=0)
 
-ref = {'Genotype Category: 14765x2':'14765x2',
-       'Genotype Category: 14763x7':'14763x7', 'Genotype Category: 14799x1':'14799x1',
-       'Genotype Category: 14858x3':'14858x3', 'Genotype Category: 14746x8':'14746x8',
-       'Genotype Category: 14824x13':'14824x13', 'Genotype Category: 14739x13':'14739x13',
-       'Genotype Category: 14704x7':'14704x7', 'Genotype Category: 14710x6':'14710x6',
-       'Genotype Category: 14781x16':'14781x16', 'Genotype Category: 1601':'1601',
-       'Genotype Category: 1401':'1401', 'Genotype Category: 902':'902',
-       'Genotype Category: 901':'901', 'Genotype Category: 1001':'1001',
-       'Genotype Category: 110':'110', 'Genotype Category: 111':'111',
-       'Genotype Category: 112':'112', 'Genotype Category: 114':'114',
-       'Genotype Category: 115':'115', 'Genotype Category: 116':'116',
-       'Genotype Category: 117':'117', 'Genotype Category: GM23716':'GM23716',
-       'Genotype Category: GM23720':'GM23720', 'Genotype Category: GM25256':'GM25256',
-       'Genotype Category: PGP1':'PGP1'}
+ref = {'110-1':'BXS0110-1','110-2':'BXS0110-2','110-3':'BXS0110-3',
+		'111-1':'BXS0111-1','111-2':'BXS0111-2','111-3':'BXS0111-3',
+       '112-1':'BYS0112-1','112-2':'BYS0112-2','112-3':'BYS0112-3',
+       '114-1':'BXS0114-1','114-2':'BXS0114-2','114-3':'BXS0114-3',
+       '115-1':'BXS0115-1','115-2':'BXS0115-2','115-3':'BXS0115-3',
+       '116-1':'BXS0116-1','116-2':'BXS0116-2','116-3':'BXS0116-3',
+       '117-1':'BXS0117-1','117-2':'BXS0117-2','117-3':'BXS0117-3'}
 
 df.rename(columns=ref, inplace=True)
 chrmPos = []
@@ -25,6 +18,10 @@ for i in range(df.shape[0]):
 	s = str(df.CHROM[i]) + "@" + str(df.POS[i])
 	chrmPos.append(s)
 
-df.append(chrmPos)
-df = df.set_index()
-print(df.head())
+df.insert(loc=0, column='Samples', value=chrmPos)
+
+df = df.drop(['CHROM','POS'], axis=1)
+
+df_t=df.T
+print(df_t.head())
+df_t.to_csv(r'SNP_data_T.csv')
