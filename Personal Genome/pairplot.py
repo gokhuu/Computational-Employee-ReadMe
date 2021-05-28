5c45d86843ffc53055352f2cb331732969e821ad
Brandon Khuu <khuub@uci.edu>
2021-05-28 17:42:28 -0400

    Update pairplot.py

--- Personal Genome/pairplot.py
+++ Personal Genome/pairplot.py
@@ -1,11 +1,11 @@
 #!/bin/python
 
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
+import numpy as np 
+import pandas as pd 
+import matplotlib.pyplot as plt 
 import seaborn as sns
 
-df = pd.read_csv('new_fpkm_threshold.csv', header=0, index_col=0, low_memory=False)
+df = pd.read_csv('fpkm_corr.csv', header=0, index_col=0, low_memory=False)
+fig = sns.pairplot(df, hue='Autism')
+fig.savefig('output.png')
 
-sns.pairplot(df, hue='Autism')
-plt.savefig('Pairplot_fpkm.png')
